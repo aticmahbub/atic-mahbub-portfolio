fc2ecc7877d2ea9be92dd38c2b3f3912bb2ec18d
aticcmahbub <aticcmahbub@gmail.com>
2024-07-27 02:37:16 +0600

    project setup and navbar done

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return <section className='h-full'>
+    <div className="container mx-auto h-full">
+      <div className='flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24'>
+        <div className=''>Text</div>
+        <div>Photo</div>
+      </div>
+    </div>
+  </section>
+    ;
+};
+
+export default Home;
---
+++ app/work/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const work = () => {
+    return (
+        <div>
+            Work
+        </div>
+    );
+};
+
+export default work;
