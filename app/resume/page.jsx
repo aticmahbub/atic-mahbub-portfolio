a8e5145597d00922a72f6e60c0f06c6a3e31fe31
aticcmahbub <aticcmahbub@gmail.com>
2024-08-19 23:18:04 +0600

    about, experience and education added

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -2,7 +2,71 @@
 
 import { FaHtml5, FaCss3, FaNodeJs, FaReact,FaGithub, } from "react-icons/fa";
 import {SiTailwindcss, SiMongodb, SiJavascript} from 'react-icons/si'
+
+
 const Resume = () => {
+    const about = {
+        title: "About me",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor natus corrupti praesentium numquam nesciunt. Vel libero ex velit eum in.",
+        info: [
+        {
+            fieldName: "Name",
+            fieldValue: "Atic Mahbub"
+        },
+        
+        {
+            fieldName: "Age",
+            fieldValue: "26"
+        },
+        {
+            fieldName: "Location",
+            fieldValue: "Bangladesh"
+        },
+        {
+            fieldName: "Phone",
+            fieldValue: "+8801795849206"
+        },
+        {
+            fieldName: "Email",
+            fieldValue: "aticcmahbub@gmail.com"
+        },
+        {
+            fieldName: "Profession",
+            fieldValue: "Web Developer"
+        },
+        {
+            fieldName: "Education",
+            fieldValue: "Hons in Development Studies"
+        },
+
+        ]
+    }
+
+    const experience = {
+        icon: '/assets/resume/badge.svg',
+        title: 'My experience',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error beatae blanditiis vero voluptatum. Tempore quod mollitia esse sint beatae at!",
+        items: [
+            {
+                company: "SofolIT",
+                position: "MERN Developer",
+                duration: "2023- Present"
+            }
+        ]
+    }
+
+    const education = {
+        icon: '/assets/resume/cap.svg',
+        title: 'Education',
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error beatae blanditiis vero voluptatum. Tempore quod mollitia esse sint beatae at!",
+        items: [
+            {
+                institution: "Hajee Mohammad Danesh Science and Technology University, Dinajpur",
+                degree: "Hons in Development Studies",
+                passing: "2024"
+            }
+        ]
+    }
     return (
         <div>
             Resume
