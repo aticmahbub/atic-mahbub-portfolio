40a85afd6f6bb674f54f67e87e4e9a02692961e2
aticcmahbub <aticcmahbub@gmail.com>
2024-08-19 22:58:34 +0600

    skills section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+"use client"
 
+import { FaHtml5, FaCss3, FaNodeJs, FaReact,FaGithub, } from "react-icons/fa";
+import {SiTailwindcss, SiMongodb, SiJavascript} from 'react-icons/si'
 const Resume = () => {
     return (
         <div>
