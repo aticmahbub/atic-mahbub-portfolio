f377ec37a8902e947a3628ac030e8601f3aac29f
aticcmahbub <aticcmahbub@gmail.com>
2024-08-19 20:06:05 +0600

    doing services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+"use client"
+import { BsArrowDownRight } from 'react-icons/bs'
+import { Link } from 'next/link';
+import { easeIn,motion } from 'framer-motion';
+
 
 const Services = () => {
+
+    const services = [
+        {
+            num: "01",
+            title: "Web Development",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet culpa nemo fuga delectus ad itaque modi dolorum. Tenetur, minus.",
+            href: "vgbhn"
+        },
+        {
+            num: "02",
+            title: "UI/UX Designer",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet culpa nemo fuga delectus ad itaque modi dolorum. Tenetur, minus.",
+            href: "kmjkm"
+        },
+        {
+            num: "03",
+            title: "SEO",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet culpa nemo fuga delectus ad itaque modi dolorum. Tenetur, minus.",
+            href: "njmkl"
+        },
+        {
+            num: "04",
+            title: "Security Researcher",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eveniet culpa nemo fuga delectus ad itaque modi dolorum. Tenetur, minus.",
+            href: "bhnjkm"
+        },
+    ]
     return (
-        <div>
-            Services
-        </div>
+        <section className='min-h-[80] flex flex-col justify-center py-12 xl:py-0'>
+            <div className='container mx-auto'>
+                <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+                className='grid grid-cols-1 md:grid-cols-2 gap-[60px]'
+                >
+                    {
+                        services.map((service, index) => {
+                            return <div 
+                            key={index}
+                            className='flex-1 flex flex-col justify-center gap-6 group'
+                            >
+                                <div className='w-full justify-between flex items-center'>
+                                    <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500'>
+                                        {service.num}</div>
+                                        <a href={service.href}><BsArrowDownRight/></a>
+                                        {/* <h2>hiii</h2> */}
+                                    </div>
+                                <h2>{service.title}</h2>
+                                <p>{service.description}</p>
+                                <div className='border-b border-white/20 w-full'></div>
+                            </div>
+                        })
+                    }
+                </motion.div>
+            </div>
+        </section>
     );
 };
 
