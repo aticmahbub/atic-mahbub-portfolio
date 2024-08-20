230efd898bc27679dd8ab95d8450dc6f0c71aa1b
aticcmahbub <aticcmahbub@gmail.com>
2024-08-20 23:58:40 +0600

    ui for resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -2,8 +2,10 @@
 
 import { FaHtml5, FaCss3, FaNodeJs, FaReact,FaGithub, } from "react-icons/fa";
 import {SiTailwindcss, SiMongodb, SiJavascript, SiFirebase} from 'react-icons/si'
-
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
+import {animate, easeIn, motion} from 'framer-motion'
 const Resume = () => {
     const about = {
         title: "About me",
@@ -112,10 +114,20 @@ const Resume = () => {
 
         ]
     }
+
+
+
     return (
-        <div>
-            Resume
-        </div>
+        <motion.div
+        initial={{opacity:0}}
+        animate={{
+            opacity:1,
+            transition:{delay:2.4, duration: 0.4, ease:easeIn}
+        }}
+        className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+        >
+            <div className="container mx-auto"></div>
+            </motion.div>
     );
 };
 
