e74d488db1cc36b2e240f7f4a52a39f923c66881
aticcmahbub <aticcmahbub@gmail.com>
2024-08-23 23:33:17 +0600

    started again

--- app/page.jsx
+++ app/page.jsx
@@ -20,27 +20,29 @@ const Home = () => {
           </p>
           {/* button and socials */}
           <div className='flex flex-col xl:flex-row items-center gap-2'>
-          <Button variant="outline"
-                  size="lg"
-                  className="uppercase flex  items-center gap-2"
-              >
-            <span>Download CV</span>
-            <FiDownload className='text-xl'/>
-          </Button>
-          <div className='mb-8 xl:mb-0'>
-            <Social 
-            containerStyles="flex gap-6"
-            iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent hover:bg-accent hover:text-primary hover:transition-all duration-500"
-            
-            />
-          </div>
+            <Button
+              href={'https://drive.google.com/file/d/1q-pbQOR_GyZ31HkOYqtvmq0fiRIdCL4G/view?usp=drive_link'}
+              variant="outline"
+              size="lg"
+              className="uppercase flex  items-center gap-2"
+            >
+              <span>Download CV</span>
+              <FiDownload className='text-xl' />
+            </Button>
+            <div className='mb-8 xl:mb-0'>
+              <Social
+                containerStyles="flex gap-6"
+                iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent hover:bg-accent hover:text-primary hover:transition-all duration-500"
+
+              />
+            </div>
           </div>
         </div>
         <div className='order-1 xl:order-none '>
-          <Photo/>
+          <Photo />
         </div>
       </div>
-          <Stats/>
+      <Stats />
     </div>
   </section>
     ;
